ed00be5b5127f33e65196bede7f3e264d732bf58
femi abimbola <femiabimbola6@gmail.com>
2025-06-30 13:31:39 -0700

    added the get

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,12 +3,13 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Express with TypeScript Server");
+});
 
 app.listen(port, async () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
-}
-)
+});
