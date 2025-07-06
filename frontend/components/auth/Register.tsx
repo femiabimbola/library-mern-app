80ee37848223c93bd4f01dcdf4320db79ddf7358
femi abimbola <femiabimbola6@gmail.com>
2025-07-06 09:48:56 -0700

    working on the register form

--- frontend/components/auth/Register.tsx
+++ frontend/components/auth/Register.tsx
@@ -1,40 +1,72 @@
+"use client"
+
 import {
-  Form, FormControl,  FormField, FormItem, FormLabel,
-  FormMessage,} from "@/components/ui/form";
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { DefaultValues, FieldValues, Path, SubmitHandler, useForm,UseFormReturn} from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { Input } from "../ui/input";
 
 export const signUpSchema = z.object({
   fullName: z.string().min(3),
   email: z.string().email(),
-  universityId: z.coerce.number(),//turns a string to a number
+  universityId: z.coerce.number(), //turns a string to a number
   universityCard: z.string().nonempty("University Card is required"),
   password: z.string().min(8),
 });
 
-
 const Register = () => {
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      fullName: "",
+      universityId: 0,
+      universityCard: "",
+    },
+  });
 
-  // const form: UseFormReturn<T> = useForm({
-  //   resolver: zodResolver(signUpSchema),
-  //   // defaultValues: defaultValues as DefaultValues<T>,
-  // });
-
+  const onSubmit = (values: z.infer<typeof signUpSchema>) => {};
 
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-semibold text-white">
-         Create your library account
+        Create your library account
       </h1>
-      <p className="text-white-200">
-        Please complete all fields and upload a valid university ID to gain access to the library
+      <p className="text-white/80">
+        Please complete all fields and upload a valid university ID to gain
+        access to the library
       </p>
-      {/* <Form {...form}>
-
-      </Form> */}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+              control={form.control}
+              name="fullName"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      // disabled={isPending}
+                      placeholder="Enter your name"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+        </form>
+      </Form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
