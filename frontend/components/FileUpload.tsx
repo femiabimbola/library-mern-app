99ff716b56363a92cb92debe026de67289c75d4a
femi abimbola <femiabimbola6@gmail.com>
2025-07-08 22:59:26 -0700

    working with imagekit upload

--- frontend/components/FileUpload.tsx
+++ frontend/components/FileUpload.tsx
@@ -1,3 +1,8 @@
+"use client"
+
+// import { IKImage, IKVideo, ImageKitProvider, IKUpload } from "imagekitio-next";
+import { Image, ImageKitProvider } from '@imagekit/next';
+
 const FileUpload = () => {
   return (
     <div> The file upload</div>
