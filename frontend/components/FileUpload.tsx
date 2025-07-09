a06c29e75495ba9bd6b65d6b6799c1ab7286fd36
femi abimbola <femiabimbola6@gmail.com>
2025-07-09 14:29:58 -0700

    updating the image upload

--- frontend/components/FileUpload.tsx
+++ frontend/components/FileUpload.tsx
@@ -3,6 +3,16 @@
 // import { IKImage, IKVideo, ImageKitProvider, IKUpload } from "imagekitio-next";
 import { Image, ImageKitProvider } from '@imagekit/next';
 
+const imageKitEndpoint = process.env.NEXT_PUBLIC_API_ENDPOINT!
+
+const authenticator = async () => {
+  try {
+    const  response = await fetch(`${imageKitEndpoint}/api/auth/imagekit`)
+  } catch (error) {
+    
+  }
+}
+
 const FileUpload = () => {
   return (
     <div> The file upload</div>
