076c00d766a7d3ea38218e69bf87d0f4aec33526
femi abimbola <femiabimbola6@gmail.com>
2025-07-20 14:53:24 -0700

    worked on the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,9 @@
 import express, { Express, Request, Response } from "express";
+import { db } from "./database/connectdb";
+import { config } from "./lib/config/app.config";
+// import dotenv from "dotenv"
+
+// dotenv.config()
 
 const app: Express = express();
 const port = process.env.PORT || 5000;
@@ -6,10 +11,15 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Express with TypeScript Server");
 });
 
 app.listen(port, async () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port} in ${process.env.NODE_ENV}`);
+  await db.execute('select 1')
+  .then(() => console.log("Database successfully connected"))
+  .catch(() => console.log("database could not successfully connect"))
 });
