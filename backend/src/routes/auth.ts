9f56d12f7fdcec2dd585be39bcd16b0747c2b06a
femi abimbola <femiabimbola6@gmail.com>
2025-07-27 23:43:01 -0700

    working on the zod validator

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,8 +1,14 @@
-import { Router } from "express";
-import authRouter from "./auth";
+import { Request, NextFunction, Router } from "express";
+import { createUser, } from "../controller/auth";
+// import {  checkSchema } from "express-validator";
+// import { createUserValidationSchema, loginUserValidationSchema } from "../validator/expressValidator";
 
 const router = Router();
 
-router.use(authRouter);
+
+// router.post("/auth/register", checkSchema(createUserValidationSchema), createUser)
+
+// router.post("/auth/login", checkSchema(loginUserValidationSchema), loginUser)
+
 
 export default router;
