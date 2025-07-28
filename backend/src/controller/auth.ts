eeec3034c51e68c4c6a45abce29ce9aef0fa4abb
femi abimbola <femiabimbola6@gmail.com>
2025-07-27 23:05:16 -0700

    added the routes dir

--- backend/src/controller/auth.ts
+++ backend/src/controller/auth.ts
@@ -3,16 +3,15 @@ import { users } from "../database/schema";
 import { eq } from "drizzle-orm";
 import { db } from "../database/connectdb";
 
-export const createUser = async (
-  req: Request, res: any, next: NextFunction
+export const createUser = async ( req: Request, res: any, next: NextFunction
 ) => {
   const { fullName, email, password, universityCard, universityId} = req.body
   try {
     const existingUser = await db
-    .select()
-    .from(users)
-    .where(eq(users.email, email))
+    .select().from(users) .where(eq(users.email, email))
     .limit(1);
+
+    if (existingUser.length > 0) { }
   } catch (error) {
     
   }
