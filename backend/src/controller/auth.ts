7e0556e32ddcbeeb2ec9bf5a1c27b7ce1cbfd113
femi abimbola <femiabimbola6@gmail.com>
2025-07-31 22:56:32 -0700

    rounding up create user

--- backend/src/controller/auth.ts
+++ backend/src/controller/auth.ts
@@ -4,20 +4,25 @@ import { eq } from "drizzle-orm";
 import { db } from "../database/connectdb";
 import { hash, compare } from "bcryptjs";
 
-export const createUser = async ( req: Request, res: any, next: NextFunction
+export const createUser = async (
+  req: Request, res: any, next: NextFunction
 ) => {
-
-  const { fullName, email, password, universityCard, universityId} = req.body
+  const { fullName, email, password, universityCard, universityId } = req.body;
 
   try {
-    const existingUser = await db.select().from(users).where(eq(users.email, email)).limit(1);
-    console.log( fullName, email, password, )
-    if (existingUser.length > 0) return res.status(401).send({ message: "User already exist" })
+    const existingUser = await db .select().from(users).where(eq(users.email, email)).limit(1);
+
+    if (existingUser.length > 0) return res.status(401).send({ message: "User already exist" });
     const hashedPassword = await hash(password, 10);
-    // const newUser = await db.insert(users).values({})
-    // console.log( fullName, email, password, )
+
+    const newUser = await db
+      .insert(users)
+      .values({
+        fullName, email, password: hashedPassword, universityCard, universityId,
+      });
+
     return res.status(201).send({ msg: "You have successfully created" });
   } catch (error) {
     return res.status(404).send({ msg: "Could not create user" });
   }
-}
+};
