d73e61cfcc52a2092fd4d4869be8f5d0ace28e48
femi abimbola <femiabimbola6@gmail.com>
2025-08-12 23:29:14 -0700

    create book controller

--- backend/src/controller/books.ts
+++ backend/src/controller/books.ts
@@ -1,8 +1,52 @@
 import { Request, Response, NextFunction } from "express";
+import { db } from "../database/connectdb";
+import { books } from "../database/schema";
 
 
 export const createBooks = async (
   req: Request, res: any, next: NextFunction
 ) => {
-  const {  } = req.body;
+
+  try {
+    const {
+      title, author, genre, rating, coverUrl,
+      coverColor,description,
+      totalCopies,
+      videoUrl,
+      summary
+    } = req.body;
+
+    // Validate required fields
+    if (!title || !author || !totalCopies) {
+      return res.status(400).json({
+        success: false,
+        message: 'Title, author, and totalCopies are required'
+      });
+    }
+
+    const newBook = await db
+      .insert(books)
+      .values({
+        title,
+        author,
+        genre,
+        rating,
+        coverUrl,
+        coverColor,
+        description,
+        totalCopies,
+        videoUrl,
+        summary,
+        availableCopies: totalCopies
+      })
+      .returning();  //it returns the book
+
+      res.status(201).json({success: true,data: newBook[0]});
+  } catch (error) {
+    console.error('Error creating book:', error);
+    res.status(500).json({
+      success: false,
+      message: 'An error occurred while creating the book'
+    });
+  }
 }
