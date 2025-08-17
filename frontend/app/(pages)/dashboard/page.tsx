cb19f5c3dd8aacafca496f61d273ec7cbbf851c9
femi abimbola <femiabimbola6@gmail.com>
2025-08-17 10:10:46 -0700

    working on the dashboard

--- frontend/app/(pages)/dashboard/page.tsx
+++ frontend/app/(pages)/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import useSWR from 'swr';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import axios, { AxiosResponse } from 'axios';
 
 // Fetcher function for SWR
 const fetcher = async (url: string) => {
@@ -13,16 +14,29 @@ const fetcher = async (url: string) => {
   return res.json();
 };
 
+const fetcher2 = async <T = unknown>(url: string): Promise<T>=> {
+  const res: AxiosResponse<T> = await axios.get(url, { withCredentials: true });
+  if (res.status < 200 || res.status >= 300) {
+    throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
+  }
+
+  return res.data;
+}
+
+
 const  UserDashboard = () => {
   const router = useRouter();
-  const { data, error, isLoading } = useSWR('http://localhost:5000/api/auth/user', fetcher);
+  const { data, error, isLoading } = useSWR('http://localhost:5000/api/user', fetcher);
+
+  console.log("Loading ->", isLoading)
+  console.log("error ->", error)
 
   // Redirect to login if not authenticated
-  useEffect(() => {
-    if (error) {
-      router.push('/auth/login'); // Redirect to login page if not authenticated
-    }
-  }, [error, router]);
+  // useEffect(() => {
+  //   if (error) {
+  //     router.push('/auth/login'); // Redirect to login page if not authenticated
+  //   }
+  // }, [error, router]);
 
   if (isLoading) {
     return <div>Loading...</div>;
