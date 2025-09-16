f9c7ede4fd7326a5038bbb85192b94a4712b9f95
femi abimbola <femiabimbola6@gmail.com>
2025-09-15 23:11:16 -0700

    fetching books

--- frontend/app/books/page.tsx
+++ frontend/app/books/page.tsx
@@ -1,10 +1,18 @@
+import { useBookStore } from "@/store/bookStore";
 import axios from "axios";
+import { useEffect } from "react";
 
 const Books = async () => {
   const BookFetcher = async (url: string) => {
     const response = await axios.get(url);
     return response.data;
   };
+
+  const { books, cart, isLoading, error, fetchBooks, addBook, addToCart, removeFromCart } = useBookStore();
+
+  useEffect(() => {
+    fetchBooks();
+  }, [fetchBooks]);
 
   return (
     <section
