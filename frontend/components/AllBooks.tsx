5f5514feb5201de959bbfec2f158ea074faac5fc
femi abimbola <femiabimbola6@gmail.com>
2025-10-02 21:33:47 -0700

    added the pages

--- frontend/components/AllBooks.tsx
+++ frontend/components/AllBooks.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { useBookStore } from "@/store/bookStore";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import ErrorBoundaryAdapter from "./GlobalErrorFallback";
+import { Button } from "./ui/button";
 
 const AllBooks = () => {
   const { books, isLoading, error, fetchBooks } = useBookStore();
@@ -18,21 +19,37 @@ const AllBooks = () => {
     <ErrorBoundary FallbackComponent={ErrorBoundaryAdapter}>
       <Card className="my-10">
         <CardHeader>
-          <CardTitle> List of all the books</CardTitle>
-          <CardDescription> These are the lists of your books</CardDescription>
+          <CardTitle className=""> List of all the books</CardTitle>
+          <CardDescription> These are the lists of your books </CardDescription>
           <Table>
             <TableHeader>
-              <TableRow>
-                <TableHead className="w-[100px]">Invoice</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Method</TableHead>
-                <TableHead className="text-right">Amount</TableHead>
+              <TableRow className="h-12 border-b">
+                <TableHead>Title</TableHead>
+                <TableHead>Author</TableHead>
+                <TableHead>Genre</TableHead>
+                <TableHead>Total Copies</TableHead>
+                <TableHead>Rating</TableHead>
+                <TableHead>Actions</TableHead>
               </TableRow>
             </TableHeader>
+            <TableBody>
+              {books.map((book) => (
+                <TableRow key={book.id}>
+                  <TableCell className="">{book.title}</TableCell>
+                  <TableCell className="">{book.author}</TableCell>
+                  <TableCell className="">{book.genre}</TableCell>
+                  <TableCell className="">{book.totalCopies}</TableCell>
+                  <TableCell className="">{book.rating}</TableCell>
+                  <TableCell className="">
+                    <div className="flex gap-x-2">
+                      <Button className="outline"> Edit</Button>
+                      <Button className="outline"> Delete</Button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
           </Table>
-          {books.map((book) => (
-            <p key={book.id}>{book.title}</p>
-          ))}
         </CardHeader>
       </Card>
     </ErrorBoundary>
