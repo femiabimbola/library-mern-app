85dc1dbc2117ffb028a159458e5191f610d4926e
femi abimbola <femiabimbola6@gmail.com>
2025-11-07 23:55:28 -0800

    started the book record

--- frontend/components/BorrowBookRecord.tsx
+++ frontend/components/BorrowBookRecord.tsx
@@ -1,7 +1,18 @@
 import ErrorBoundaryAdapter from "./GlobalErrorFallback";
+import { ErrorBoundary } from "react-error-boundary";
+import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
 
 const BorrowBookRecord = () => {
-  return <p>The borrow BOOK pAGE</p>;
+  return (
+    <ErrorBoundary FallbackComponent={ErrorBoundaryAdapter}>
+      <Card className="my-10">
+        <CardHeader>
+          <CardTitle className=""> List of all the books</CardTitle>
+          <CardDescription> These are the lists of your books </CardDescription>
+        </CardHeader>
+      </Card>
+    </ErrorBoundary>
+  );
 };
 
 export default BorrowBookRecord;
