4bee687e9bc5bb342f1c0737ad285f2f3e6cd054
femi abimbola <femiabimbola6@gmail.com>
2025-11-08 09:53:21 -0800

    building the all borrow book record

--- frontend/components/BorrowBookRecord.tsx
+++ frontend/components/BorrowBookRecord.tsx
@@ -1,14 +1,33 @@
 import ErrorBoundaryAdapter from "./GlobalErrorFallback";
 import { ErrorBoundary } from "react-error-boundary";
+import { useCallback, useEffect, useState } from "react";
 import { Card, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import useSWRMutation from "swr/mutation";
+import axios from "axios";
+
+const getBorrowRecord = async (url: string) => {
+  const response = await axios.get(url);
+  return response.data;
+};
 
 const BorrowBookRecord = () => {
+  const {
+    trigger,
+    isMutating,
+    error: swrError,
+  } = useSWRMutation(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/borrow`, getBorrowRecord);
+
+  useEffect(() => {
+    trigger();
+  });
+
   return (
     <ErrorBoundary FallbackComponent={ErrorBoundaryAdapter}>
       <Card className="my-10">
         <CardHeader>
           <CardTitle className=""> List of all the books</CardTitle>
-          <CardDescription> These are the lists of your books </CardDescription>
+          <CardDescription> These are the lists of the borrow record </CardDescription>
         </CardHeader>
       </Card>
     </ErrorBoundary>
