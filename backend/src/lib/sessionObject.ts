2564597aa409c3a321cc901043e385c32b2e4a8f
femi <femiabimbola6@gmail.com>
2026-01-24 03:28:34 -0800

    refactoring the codes

--- backend/src/lib/sessionObject.ts
+++ backend/src/lib/sessionObject.ts
@@ -3,20 +3,6 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
-// export const sessionObject = {
-//   secret: "some tough secret", // to sign the cookies
-//   saveUninitialized: false, // Not saving unmodified session storage. No need to save random user
-//   resave: false,  //if session data is not modified, why store?
-//   proxy: true,
-//   cookie: {
-//     httpOnly: true,
-//     secure: process.env.NODE_ENV === 'production',   // true on prod
-//     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax" as 'none' | 'lax',
-//     maxAge: 1000 * 60 * 60 * 24, // 1 day
-//   }
-// }
-
-
 export const sessionObject2 = {
   name: 'connect.sid',
   secret: process.env.SESSION_SECRET!,
@@ -26,8 +12,7 @@ export const sessionObject2 = {
   cookie: {
     httpOnly: true,
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax" as 'none' | 'lax',
-    
-    secure: process.env.NODE_ENV === 'production',   // true on prod
+    secure: process.env.NODE_ENV === 'production', 
     maxAge: 1000 * 60 * 60 * 24 // 24 hours
   }
 }
